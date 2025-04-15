81bc3c8bb57100989b91d24c6b5f760f9c65cbba
季树朋 <1778849047@qq.com>
2025-04-15 09:54:06 +0800

    update Nacos config

--- intelligent-office-ds/ds-api/src/main/java/com/thtf/chat/ChatApiApplication.java
+++ intelligent-office-ds/ds-api/src/main/java/com/thtf/chat/ChatApiApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan(basePackages = {"com.thtf.chat.mapper"})
 @EnableAsync // 开启异步
 @EnableScheduling // 开启定时任务
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.thtf.feign.client")
 public class ChatApiApplication {
 

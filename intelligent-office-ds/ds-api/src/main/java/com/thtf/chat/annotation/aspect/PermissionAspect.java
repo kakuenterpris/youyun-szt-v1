115ecd421997204459e0faf5cd2a83ef3022c4bf
季树朋 <1778849047@qq.com>
2025-05-28 22:08:08 +0800

    优化切面权限检查逻辑及异常处理

--- intelligent-office-ds/ds-api/src/main/java/com/thtf/chat/annotation/aspect/PermissionAspect.java
+++ intelligent-office-ds/ds-api/src/main/java/com/thtf/chat/annotation/aspect/PermissionAspect.java
@@ -12,6 +12,7 @@ import com.thtf.global.common.dto.SystemUser;
 import com.thtf.global.common.rest.DefaultErrorCode;
 import com.thtf.global.common.rest.RestResponse;
 import com.thtf.global.common.utils.JsonUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
@@ -19,6 +20,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -26,6 +28,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 
@@ -45,23 +48,18 @@ public class PermissionAspect {
     public Object checkPermission(ProceedingJoinPoint joinPoint, RequiresPermission requiresPermission) throws Throwable {
         String token = getTokenFromContext();
         if (token == null || token.isEmpty()) {
-            throw new RuntimeException("Token is missing");
+            throw new RuntimeException("Token获取失败");
         }
         try {
             String userInfoStr = (String) redisUtil.get("token_" + token);
             if (StringUtils.isEmpty(userInfoStr)){
-                if (joinPoint.proceed() instanceof SseEmitter){
-                    SseEmitter sseEmitter = new SseEmitter((long) Integer.MAX_VALUE);
-                    sseEmitter.send(DefaultErrorCode.INVALID_TOKEN);
-                    return sseEmitter;
-                }
                 return RestResponse.fail(DefaultErrorCode.INVALID_TOKEN);
             }
             SystemUser systemUser = JsonUtil.fromJson(userInfoStr, SystemUser.class);
             //查询用户的角色和权限
             String requiredPermission = requiresPermission.value();
             boolean hasPermission = false;
-            List<SysRoleEntity> roleByUserId = sysRoleRepo.getRoleByUserId(Integer.valueOf(systemUser.getUserId()));
+            List<SysRoleEntity> roleByUserId = sysRoleRepo.getRoleByUserId(Integer.valueOf(systemUser.getId()));
             //检查用户权限
             for (SysRoleEntity role : roleByUserId) {
                 List<SysMenuEntity> menuByRoleId = sysMenuRepo.getMenuByRoleId(role.getRoleId());
@@ -72,13 +70,8 @@ public class PermissionAspect {
                     }
                 }
             }
-            if (!hasPermission) {
-                if (joinPoint.proceed() instanceof SseEmitter){
-                    SseEmitter sseEmitter = new SseEmitter((long) Integer.MAX_VALUE);
-                    sseEmitter.send(DefaultErrorCode.INVALID_TOKEN);
-                    return sseEmitter;
-                }
-                return RestResponse.fail(DefaultErrorCode.INVALID_TOKEN);
+            if (!hasPermission&&!"SYSTEM_MANAGE".equals(systemUser.getSpecialAuth())) {
+                return RestResponse.fail(DefaultErrorCode.PERMISSION_DENIED);
             }
                 return joinPoint.proceed();
         }catch (Exception e) {
@@ -88,6 +81,16 @@ public class PermissionAspect {
 
     private String getTokenFromContext() {
         HttpServletRequest request = getHttpServletRequest();
+        //        获取cookie 中的sessionId
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {  // 假设token存储在名为"token"的cookie中
+                    System.out.println("cookie:" + cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
         // 从header取
         String fromHeader = request.getHeader(AuthConstants.token_key);
         if (StringUtils.isNotBlank(fromHeader)){
@@ -108,5 +111,15 @@ public class PermissionAspect {
             return attributes.getRequest();
         }
         throw new IllegalStateException("HttpServletRequest is not available in the current context");
+    }
+
+    private boolean isSseEmitterResponse(ProceedingJoinPoint joinPoint) {
+        try {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = signature.getMethod();
+            return SseEmitter.class.isAssignableFrom(method.getReturnType());
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
